7857067e402a100af8043c50ab78e7d15759b03f
Yejin Song <songyejin2000@gmail.com>
2022-11-23 03:09:30 +0900

    feat: add logoutBtn

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,8 @@ const Menu = () => {
                 <Link to="/">수강 신청 강의 목록 확인</Link>
                 <br/>
                 <Link to="/CourseEnrollList">수강 신청 내역 조회</Link>
+                <br/><br/>
+                <button>로그아웃</button>
             </div>
         </div>
     )
